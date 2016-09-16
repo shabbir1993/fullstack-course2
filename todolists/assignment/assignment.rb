00eaf08b8e648069af23d9ad923c6a1bcbeeebbb
shabbir1993 <syed_shabbir_haider@hotmail.com>
2016-09-16 22:28:55 +0500

    Implemented Creat Users and TodoList

--- todolists/assignment/assignment.rb
+++ todolists/assignment/assignment.rb
@@ -10,12 +10,15 @@ class Assignment
       # accept a hash of user properties (`:username` and `:password_digest`) as an input parameter. Note these are 100% same as model class.
       # use the User Model class to create a new user in the DB
       # return an instance of the class with primary key (`id`), and dates (`created_at` and `updated_at`) assigned
+      User.create(params)
   end
 
   def create_todolist(params)
       # accept a hash of todolist properties (`:name` and `:due_date`) as an input parameter. Note these are not 100% the same as Model class.
       # use the TodoList Model class to create a new user in the DB
       # return an instance of the class with primary key (`id`), and dates (`created_at` and `updated_at`) assigned
+      ln = {list_name: params[:name], list_due_date: params[:due_date]}
+      TodoList.create!(ln)
   end
 
   #
